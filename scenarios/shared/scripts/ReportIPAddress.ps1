5869e45e524b2aa8054d3565e9a52606ce21c30e
tpetchel <tpetchel@gmail.com>
2017-07-26 07:48:50 -0400

    Improve how Windows reports its IP address

--- scenarios/shared/scripts/ReportIPAddress.ps1
+++ scenarios/shared/scripts/ReportIPAddress.ps1
@@ -1,1 +1,27 @@
-(Get-NetIPAddress -AddressState Preferred -PrefixOrigin Dhcp).IPAddress | Out-File -Encoding ASCII C:\vagrant\$($env:COMPUTERNAME.ToLower())-ipaddress.txt
+$Stoploop = $false
+[int]$Retrycount = "0"
+
+do {
+  try {
+    $name = $env:COMPUTERNAME.ToLower()
+    Write-Host "Computer name is $name"
+
+    $ip_addr = (Get-NetIPAddress -AddressState Preferred -PrefixOrigin Dhcp).IPAddress
+    Write-Host "IP address is $ip_addr"
+
+    $ip_addr | Out-File -Encoding ASCII "C:\vagrant\$name-ipaddress.txt"
+    $Stoploop = $true
+    }
+  catch {
+    if ($Retrycount -gt 3){
+      Write-Host "Could not obtain IP address after 3 retries."
+      $Stoploop = $true
+    }
+    else {
+      Write-Host "Could not obtain IP address - retrying in 10 seconds..."
+      Start-Sleep -Seconds 10
+      $Retrycount = $Retrycount + 1
+    }
+  }
+}
+While ($Stoploop -eq $false)
